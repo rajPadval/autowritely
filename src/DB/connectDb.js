7437c83ba7c64b6f85d3c1b329525e09dda625c8
rajPadval <97424854+rajPadval@users.noreply.github.com>
2023-08-11 08:55:27 +0530

    Added new changes

--- src/DB/connectDb.js
+++ src/DB/connectDb.js
@@ -1,8 +1,10 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 const db = async () => {
-  
-    const mongoConnection = await mongoose.connect(process.env.MONGO_URI);
+  // Before connecting to MongoDB, set the default timeout to a higher value (e.g., 30 seconds)
+  mongoose.set("connectTimeoutMS", 50000);
+
+  const mongoConnection = await mongoose.connect(process.env.MONGO_URI);
 
   if (mongoConnection) {
     console.log("DB Connected⚡");
